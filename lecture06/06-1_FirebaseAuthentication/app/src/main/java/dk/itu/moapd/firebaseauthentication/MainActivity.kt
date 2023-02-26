8f2cdf9f2915fcee29b482cbf827061e36c6045a
Fabricio Batista Narcizo <fabricionarcizo@gmail.com>
2023-02-26 09:20:49 +0100

    Lecture #06 (Android Backend with Google Firebase)
    This commit has the Android projects used during Lecture #06 of the Mobile App Development BSc. 2023 course **on March 7th, 2023**.

---
+++ lecture06/06-1_FirebaseAuthentication/app/src/main/java/dk/itu/moapd/firebaseauthentication/MainActivity.kt
@@ -0,0 +1,198 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 Fabricio Batista Narcizo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package dk.itu.moapd.firebaseauthentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import dk.itu.moapd.firebaseauthentication.databinding.ActivityMainBinding
+
+
+/**
+ * An activity class with methods to manage the main activity of Firebase Authentication
+ * application.
+ */
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * View binding is a feature that allows you to more easily write code that interacts with
+     * views. Once view binding is enabled in a module, it generates a binding class for each XML
+     * layout file present in that module. An instance of a binding class contains direct references
+     * to all views that have an ID in the corresponding layout.
+     */
+    private lateinit var binding: ActivityMainBinding
+
+    /**
+     * The entry point of the Firebase Authentication SDK.
+     */
+    private lateinit var auth: FirebaseAuth
+
+    /**
+     * Called when the activity is starting. This is where most initialization should go: calling
+     * `setContentView(int)` to inflate the activity's UI, using `findViewById()` to
+     * programmatically interact with widgets in the UI, calling
+     * `managedQuery(android.net.Uri, String[], String, String[], String)` to retrieve cursors for
+     * data being displayed, etc.
+     *
+     * You can call `finish()` from within this function, in which case `onDestroy()` will be
+     * immediately called after `onCreate()` without any of the rest of the activity lifecycle
+     * (`onStart()`, `onResume()`, onPause()`, etc) executing.
+     *
+     * <em>Derived classes must call through to the super class's implementation of this method. If
+     * they do not, an exception will be thrown.</em>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut
+     * down then this Bundle contains the data it most recently supplied in `onSaveInstanceState()`.
+     * <b><i>Note: Otherwise it is null.</i></b>
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Migrate from Kotlin synthetics to Jetpack view binding.
+        // https://developer.android.com/topic/libraries/view-binding/migration
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Set a `Toolbar` to act as the `ActionBar` for this Activity window.
+        setSupportActionBar(binding.toolbar)
+
+        // Initialize Firebase Auth.
+        auth = FirebaseAuth.getInstance()
+
+        // Define the UI components behavior.
+        binding.contentMain.apply {
+            thumbUpIcon.setOnClickListener {
+                snackBar("Liked")
+            }
+            favoriteIcon.setOnClickListener {
+                snackBar("Your Favorite")
+            }
+            shareIcon.setOnClickListener {
+                snackBar("Sharing")
+            }
+        }
+    }
+
+    /**
+     * Initialize the contents of the Activity's standard options menu.  You should place your menu
+     * items in to menu.  This is only called once, the first time the options menu is displayed.
+     * To update the menu every time it is displayed, see `onPrepareOptionsMenu(Menu)`.  The default
+     * implementation populates the menu with standard system menu items.  These are placed in the
+     * `Menu#CATEGORY_SYSTEM` group so that they will be correctly ordered with application-defined
+     * menu items.  Deriving classes should always call through to the base implementation.  You can
+     * safely hold on to menu (and any items created from it), making modifications to it as
+     * desired, until the next time `onCreateOptionsMenu()` is called.  When you add items to the
+     * menu, you can implement the Activity's `onOptionsItemSelected(MenuItem)` method to handle
+     * them there.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     * @return You must return `true` for the menu to be displayed; if you return `false` it will
+     *      not be shown.
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    /**
+     * This hook is called whenever an item in your options menu is selected.  The default
+     * implementation simply returns `false` to have the normal processing happen (calling the
+     * item's `Runnable` or sending a message to its `Handler` as appropriate).  You can use this
+     * method for any items for which you would like to do processing without those other
+     * facilities.  Derived classes should call through to the base class for it to perform the
+     * default menu handling.
+     *
+     * @param item The menu item that was selected.  This value cannot be `null`.
+     *
+     * @return Return `false` to allow normal menu processing to proceed, `true` to consume it here.
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            // Firebase Sign Out.
+            R.id.action_sign_out -> {
+                auth.signOut()
+                startLoginActivity()
+                true
+            } else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**
+     * Called after `onCreate()` method; or after `onRestart()` method when the activity had been
+     * stopped, but is now again being displayed to the user. It will usually be followed by
+     * `onResume()`. This is a good place to begin drawing visual elements, running animations, etc.
+     *
+     * You can call `finish()` from within this function, in which case `onStop()` will be
+     * immediately called after `onStart()` without the lifecycle transitions in-between
+     * (`onResume()`, `onPause()`, etc) executing.
+     *
+     * <em>Derived classes must call through to the super class's implementation of this method. If
+     * they do not, an exception will be thrown.</em>
+     */
+    override fun onStart() {
+        super.onStart()
+
+        // Check if the user is not logged and redirect her/him to the LoginActivity.
+        if (auth.currentUser == null)
+            startLoginActivity()
+
+        // Set the user information.
+        val user = auth.currentUser
+        binding.contentMain.description.text = getString(
+            R.string.firebase_user_description,
+            if (user?.email!!.isEmpty()) user.phoneNumber else user.email
+        )
+    }
+
+    /**
+     * This method starts the login activity which allows the user log in or sign up to the Firebase
+     * Authentication application.
+     *
+     * Before accessing the main activity, the user must log in the application through a Firebase
+     * Auth backend service. The method starts a new activity using explicit intent and used the
+     * method `finish()` to disable back button.
+     */
+    private fun startLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    /**
+     * Make a standard snack-bar that just contains text.
+     */
+    private fun snackBar(text: CharSequence,
+                         duration: Int = Snackbar.LENGTH_SHORT) {
+        Snackbar
+            .make(findViewById(R.id.content_main), text, duration)
+            .show()
+    }
+
+}
---
+++ lecture06/README.md
@@ -0,0 +1,7 @@
+# Lecture #06  (Android Backend with Google Firebase)
+
+This folder has the Android projects used during Lecture #06 of the Mobile App Development BSc. 2023 course **on March 7th, 2023**.
+
+[Firebase Authentication](06-1_FirebaseAuthentication): A simple authentication system based on Firebase UI that allows sign-in to the application via e-mail/password, phone number, or Google account.
+
+[Android Testing](06-2_AndroidTesting): An example project that shows how to test evaluating an Android app using local testing (JUnit Framework) and instrumented tests (Espresso Framework).
